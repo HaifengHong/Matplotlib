2f1f6696ede1df49992dcf0159d673efdf5c16cf
HaifengHong <37770576+HaifengHong@users.noreply.github.com>
2020-05-04 22:53:38 +0800

    Update snapshot-selenium_test.py

--- snapshot-selenium_test.py
+++ snapshot-selenium_test.py
@@ -35,7 +35,7 @@ from pyecharts.globals import ThemeType
 
 # 创建一个柱状图Bar实例
 bar = (
-    Bar(init_opts=opts.InitOpts(theme=ThemeType.WALDEN))
+    Bar(init_opts=opts.InitOpts(theme=ThemeType.LIGHT))
         # 添加X轴数据
         .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
         # 添加Y轴数据,系列的名称
@@ -43,9 +43,11 @@ bar = (
         .add_yaxis("商家B", [8, 15, 60, 20, 25, 30])
         # 添加标题
         .set_global_opts(title_opts=opts.TitleOpts(title="主标题: 双十一销量", subtitle="副标题:服饰类"))
+        # 或者直接使用字典参数（key必须是text、subtext）
+        # .set_global_opts(title_opts={"text": "主标题: 双十一销量", "subtext": "副标题:服饰类"})
 )
 
-bar.render('theme_.html')
+bar.render('theme.html')
 
 # # 输出保存为图片
 # make_snapshot(snapshot, bar.render(), "sstest20.png", pixel_ratio=20, is_remove_html=True)
