6e9df9085349e4d36f060a15cf62deb777ee878f
HaifengHong <37770576+HaifengHong@users.noreply.github.com>
2020-05-04 23:01:34 +0800

    Update Corey Schafer (Part 1)_ Creating and Customizing Our First Plots.py

--- Corey Schafer (Part 1)_ Creating and Customizing Our First Plots.py
+++ Corey Schafer (Part 1)_ Creating and Customizing Our First Plots.py
@@ -12,21 +12,18 @@ plt.xkcd()  # 漫画(comics)风格
 ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 
 # Median Developer Salaries by Age
-dev_y = [38496, 42000, 46752, 49320, 53200,
-         56000, 62316, 64928, 67317, 68748, 73752]
+dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
 plt.plot(ages_x, dev_y, color='#444444', linestyle='--', marker='*', label='All Devs')   
 # plot默认用line画出（plt.bar()画出条形图）
 # color（可传入hex value)、linestyle、marker可分开写，也可合并写（省略参数名称）
 # ['blue', 'Red', '#e5Ae37', 'GrEeN'] 表示颜色的字母大小写均可。'k'指黑色black
 
 # Median Python Developer Salaries by Age
-py_dev_y = [45372, 48876, 53850, 57287, 63016,
-            65998, 70003, 70000, 71496, 75370, 83640]
+py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
 plt.plot(ages_x, py_dev_y, 'b', linewidth=3, label='Python')
 
 # Median JavaScript Developer Salaries by Age
-js_dev_y = [37810, 43515, 46823, 49293, 53437,
-            56373, 62375, 66674, 68745, 68746, 74583]
+js_dev_y = [37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583]
 plt.plot(ages_x, js_dev_y, color='#adad3b', linewidth=3, label='JavaScript')
 
 plt.xlabel('Ages')
